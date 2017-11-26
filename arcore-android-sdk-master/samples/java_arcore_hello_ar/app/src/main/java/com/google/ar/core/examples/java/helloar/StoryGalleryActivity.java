96b9c9bac5332f3f13b7e3fb2d12d5dd2033b1d6
Teo Stoleru <teodorastoleru96@gmail.com>
2017-11-26 01:32:43 +0100

    Add story page button to gallery.

--- arcore-android-sdk-master/samples/java_arcore_hello_ar/app/src/main/java/com/google/ar/core/examples/java/helloar/StoryGalleryActivity.java
+++ arcore-android-sdk-master/samples/java_arcore_hello_ar/app/src/main/java/com/google/ar/core/examples/java/helloar/StoryGalleryActivity.java
@@ -1,7 +1,10 @@
 package com.google.ar.core.examples.java.helloar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StoryGalleryActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class StoryGalleryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story_gallery);
+
+        Button storyButton = (Button)findViewById(R.id.story_button);
+
+        storyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity();
+            }
+        });
+    }
+
+    private void launchActivity() {
+        Intent intent = new Intent(this, StoryActivity.class);
+        startActivity(intent);
     }
 }
