10b58a08da1a6d6dfc65184444f9696f33bf68a0
Parth Aggarwal <parth.aggarwal@thetrainline.com>
2017-11-26 06:06:43 +0000

    Added one geofency to StoryActivityClass

--- arcore-android-sdk-master/samples/java_arcore_hello_ar/app/src/main/java/com/google/ar/core/examples/java/helloar/StoryActivity.java
+++ arcore-android-sdk-master/samples/java_arcore_hello_ar/app/src/main/java/com/google/ar/core/examples/java/helloar/StoryActivity.java
@@ -2,10 +2,28 @@ package com.google.ar.core.examples.java.helloar;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.provider.SyncStateContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.List;
+
 public class StoryActivity extends AppCompatActivity {
+
+    private GeofencingClient mGeofencingClient;
+
+    Geofence geofence = new Geofence.Builder()
+            .setRequestId("abc") // Geofence ID
+            .setCircularRegion( 1111, 1111, 1000) // defining fence region
+            // Transition types that it should look for
+            .setTransitionTypes( Geofence.GEOFENCE_TRANSITION_EXIT)
+            .build();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +43,10 @@ public class StoryActivity extends AppCompatActivity {
     private void launchActivity() {
         Intent intent = new Intent(this, HelloArActivity.class);
         startActivity(intent);
+    }
+
+    private void launchAudio() {
+        //..............
     }
 
 }
