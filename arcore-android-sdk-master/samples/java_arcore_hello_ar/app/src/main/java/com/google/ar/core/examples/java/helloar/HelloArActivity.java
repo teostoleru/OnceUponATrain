3b78c65a0431986c9145f648a832cf4d67c9db93
Teo Stoleru <teodorastoleru96@gmail.com>
2017-11-27 20:45:39 +0000

    Update the 3D model.

--- arcore-android-sdk-master/samples/java_arcore_hello_ar/app/src/main/java/com/google/ar/core/examples/java/helloar/HelloArActivity.java
+++ arcore-android-sdk-master/samples/java_arcore_hello_ar/app/src/main/java/com/google/ar/core/examples/java/helloar/HelloArActivity.java
@@ -193,6 +193,7 @@ public class HelloArActivity extends AppCompatActivity implements GLSurfaceView.
         // Prepare the other rendering objects.
         try {
             mVirtualObject.createOnGlThread(/*context=*/this, "viking.obj", "viking.png");
+            // mVirtualObject.createOnGlThread(/*context=*/this, "PonteDeTijolos.obj", "PonteDeTijolosTextura.jpg");
             mVirtualObject.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
 
             // mVirtualObjectShadow.createOnGlThread(/*context=*/this,
